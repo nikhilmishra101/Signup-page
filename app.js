941170771812fa97415825058d54ce4cba9cac71
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-16 20:30:46 +0530

    completed the project

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-var path = require("path");
 
 const app = express();
 
-app.use("/css", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
-app.listen(3000, function (req, res) {
+
+app.post("/", function (req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us18.api.mailchimp.com/3.0/lists/8096809366",
+    method: "POST",
+    headers: {
+      Authorization: "Nick 2c7dbc103a69c840f6d19181a33a33d8-us18",
+    },
+    body: jsonData,
+  };
+  request(options, function (error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+  });
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+app.listen(process.env.PORT || 3000, function (req, res) {
   console.log("Server is running on port 3000");
 });
